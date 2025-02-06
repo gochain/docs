be90453eb839b7b6b2ba4c029925764ae76edbef
Travis Reeder <treeder@gmail.com>
2025-02-06 15:46:27 -0800

    Add install curl
    a

--- public-network/signers/nodes/setup.sh
+++ public-network/signers/nodes/setup.sh
@@ -3,6 +3,10 @@ sysctl -w vm.max_map_count=262144
 echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
 ulimit -n 100000
 echo "root             soft    nofile          100000" >> /etc/security/limits.conf
+
+
+#Docker
+apt -y install curl
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
